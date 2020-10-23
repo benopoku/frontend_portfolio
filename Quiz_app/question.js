eb83060a417efbae825cd016f18d1689f8d84896
benopoku <benzyq803@gmail.com>
2020-10-23 15:53:05 +0000

    my first javascripts assignments

---
+++ Quiz_app/question.js
@@ -0,0 +1,71 @@
+var questions = [{
+	"question": "The first mechanical computer designed by Charlse Babbage was called ?",
+	"option1": "Abatus",
+	"option2": "Analytical Engine",
+	"option3": "Calculator",
+	"option4": "Processor",
+	"answer": "2"
+}, {
+	"question": "Which of the following is the most powerful type of computrt ?",
+	"option1": "Super micro",
+	"option2": "Super conductor",
+	"option3": "Super computer",
+	"option4": "MegaFrme",
+	"answer": "3"
+}, {
+	"question": "Web pages are written using  ?",
+	"option1": "FTP",
+	"option2": "HTTP",
+	"option3": "HTML",
+	"option4": "URL",
+	"answer": "3"
+}, {
+	"question": "Who is the president of Ghana  ?",
+	"option1": "Buhari",
+	"option2": "Akuffo Addo",
+	"option3": "Mahama",
+	"option4": "John Boye",
+	"answer": "2"
+}, {
+	"question": "Who is the president of Nigeria  ?",
+	"option1": "Buhari",
+	"option2": "Akuffo Addo",
+	"option3": "Mahama",
+	"option4": "John Boye",
+	"answer": "1"
+}, {
+	"question": "What is the capital city of Ghana  ?",
+	"option1": "Accra",
+	"option2": "Kumasi",
+	"option3": "Tarkwa",
+	"option4": "Sunyani",
+	"answer": "1"
+}, {
+	"question": "Who is the president of USA  ?",
+	"option1": "Messi",
+	"option2": "Ahoufe Patri",
+	"option3": "Allata",
+	"option4": "Trump",
+	"answer": "4"
+}, {
+	"question": "Who is the president of Russia  ?",
+	"option1": "Messi",
+	"option2": "Ahoufe Patri",
+	"option3": "Putin",
+	"option4": "Trump",
+	"answer": "3"
+}, {
+	"question": "Who is the president of Rwanda  ?",
+	"option1": "Messi",
+	"option2": "Paul Kegami",
+	"option3": "Putin",
+	"option4": "Trump",
+	"answer": "2"
+}, {
+	"question": "Who is the president of Kanya  ?",
+	"option1": "Messi",
+	"option2": "Paul Kegami",
+	"option3": "Kanyanta",
+	"option4": "Trump",
+	"answer": "3"
+}];
